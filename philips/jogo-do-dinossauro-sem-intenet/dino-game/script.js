f7d8a70ba8da02f63eacc33908602ea207c7d653
Artstar10 <arthurnevesx@hotmail.com>
2022-05-14 18:00:04 -0300

    adicionando: evento tecla de espaço pressionada - parte 3

---
+++ philips/jogo-do-dinossauro-sem-intenet/dino-game/script.js
@@ -0,0 +1,8 @@
+const dino = document.querySelector('.dino');
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        console.log('Pressionou espaço!')
+    }
+}
+document.addEventListener('keyup', handleKeyUp);
