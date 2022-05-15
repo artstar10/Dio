9ffab306e0c008683a07da41f02a1421147c773c
Artstar10 <arthurnevesx@hotmail.com>
2022-05-15 00:02:12 -0300

    adicionando:  leftInterval - position cacto

--- philips/jogo-do-dinossauro-sem-intenet/dino-game/script.js
+++ philips/jogo-do-dinossauro-sem-intenet/dino-game/script.js
@@ -40,10 +40,23 @@ function jump() {
   function createCactus() {
     const cactus = document.querySelector('div');
     let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
 
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if(cactusPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20)
+
+    setTimeout(createCactus, randomTime);
   }
 
   createCactus();
