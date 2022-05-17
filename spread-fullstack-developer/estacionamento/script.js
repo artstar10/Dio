a7e293e2b75146c578d3815983b7cce4d382a350
Artstar10 <arthurnevesx@hotmail.com>
2022-05-17 15:54:49 -0300

    funcao salvar

--- spread-fullstack-developer/estacionamento/script.js
+++ spread-fullstack-developer/estacionamento/script.js
@@ -9,7 +9,7 @@
         function salvar(veiculos) {
             localStorage.setItem("patio", JSON.stringify(veiculos));
         }
-        function adicionar(veiculo) {
+        function adicionar(veiculo, salva) {
             var _a;
             const row = document.createElement("tr");
             row.innerHTML = `
@@ -21,12 +21,20 @@
             </td>
             `;
             (_a = $("#patio")) === null || _a === void 0 ? void 0 : _a.appendChild(row);
-            salvar([...ler(), veiculo]);
+            if (salva)
+                salvar([...ler(), veiculo]);
         }
         function remover() { }
-        function render() { }
+        function render() {
+            $("#patio").innerHTML = "";
+            const patio = ler();
+            if (patio.length) {
+                patio.forEach((veiculo) => adicionar(veiculo));
+            }
+        }
         return { ler, adicionar, remover, salvar, render };
     }
+    patio().render();
     (_a = $("#cadastrar")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
         var _a, _b;
         const nome = (_a = $("#nome")) === null || _a === void 0 ? void 0 : _a.value;
@@ -35,6 +43,6 @@
             alert("Os campos nome e placa são obrigatórios");
             return;
         }
-        patio().adicionar({ nome, placa, entrada: new Date() });
+        patio().adicionar({ nome, placa, entrada: new Date() }, true);
     });
 })();
