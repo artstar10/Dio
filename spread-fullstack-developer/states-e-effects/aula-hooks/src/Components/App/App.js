a592ffa06722b9d2afa45980f94871606b254bb0
Artstar10 <arthurnevesx@hotmail.com>
2022-05-18 15:07:35 -0300

    criando um componente utilizando estados

--- spread-fullstack-developer/states-e-effects/aula-hooks/src/Components/App/App.js
+++ spread-fullstack-developer/states-e-effects/aula-hooks/src/Components/App/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Counter from '../Counter/Counter';
+import SmartCounter from '../SmartCounter/SmartCounter';
 
 function App() {
   return (
     <>
-      <h1>Projeto inicial</h1>
-      <Counter />
+      <h1>Hello World</h1>
+     <SmartCounter />
     </>
   );
 }
