adec0ac08ceb98a3f08f01a0f0991ff0c189010d
Arthur Neves <arthurnevesx@hotmail.com>
2022-05-25 21:16:42 -0300

    endpoints: atualizar, deletar e cadastrar usuarios

--- spread-fullstack-developer/node/arquitetura-rest-com-node/src/routes/users.route.ts
+++ spread-fullstack-developer/node/arquitetura-rest-com-node/src/routes/users.route.ts
@@ -9,22 +9,32 @@ import { StatusCodes } from "http-status-codes";
 
 const usersRoute = Router();
 
-usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {// BUSCAR TODOS OS USUÁRIOS
     const users = [{ userName: 'Arthur' }];
     res.status(StatusCodes.OK).send({users});
 });
 
-usersRoute.get('/users/:uuid', (req: Request<{uuid: string }>, res: Response, next: NextFunction) => {
+usersRoute.get('/users/:uuid', (req: Request<{uuid: string }>, res: Response, next: NextFunction) => {//BUSCAR USUÁRIO POR ID
     const uuid = req.params.uuid;
     res.status(200).send({ uuid })
 });
 
-usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
-    const newUser = req.body;
-    
-    console.log(req.body);
-    
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {// CRIAR UM USUÁRIO
+    const newUser = req.body;  
     res.status(StatusCodes.CREATED).send(newUser);
+});
+
+usersRoute.put('/users/:uuid', (req: Request<{uuid: string }>, res: Response, next: NextFunction) => {// ATUALIZAR UM USUÁRIO
+    const uuid = req.params.uuid;
+    const modifiedUser = req.body;
+
+    modifiedUser.uuid = uuid
+
+    res.status(StatusCodes.OK).send(modifiedUser);
+});
+
+usersRoute.delete('/users/:uuid', (req: Request<{uuid: string }>, res: Response, next: NextFunction) => {// ATUALIZAR UM USUÁRIO
+    res.sendStatus(StatusCodes.OK);
 });
 
 export default usersRoute;
