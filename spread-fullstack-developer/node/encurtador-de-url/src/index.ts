d032274be31e266500bfa2f917e7e21082e302bd
Arthur Neves <arthurnevesx@hotmail.com>
2022-05-31 19:32:38 -0300

    encurtador url e conexão com mongodb
    Description: nesse commit foi realizado o desenvolvimento de short, has e conexão com o banco de dados mongodb

--- spread-fullstack-developer/node/encurtador-de-url/src/index.ts
+++ spread-fullstack-developer/node/encurtador-de-url/src/index.ts
@@ -1,9 +1,15 @@
-import express, {Request, Response} from 'express';
+import express from 'express';
+import { MongoConnection } from 'database/MongoConnection';
+import { URLController } from './controller/URLController';
 
 const api = express()
+api.use(express.json());
 
-api.use('/test', (req: Request, res: Response) => {
-    res.json({ success: true })// RETORNA JSON
-})
+const database = new MongoConnection()
+database.connect()
+
+const urlController = new URLController()
+api.post("/shorten", urlController.shorten)
+api.get("/:hash", urlController.redirect);
 
 api.listen(5000, () => console.log('Express listening'))
