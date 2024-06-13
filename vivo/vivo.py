37314b64feccd6d89320d88b6e64710a396b663b
Arthur Neves <37708374+artstar10@users.noreply.github.com>
2024-06-13 16:36:33 -0300

    variaveis_and_constantes.py
    adicionando: variáveis e cosntantes

--- vivo/vivo.py
+++ vivo/vivo.py
@@ -1,4 +1,24 @@
 //back-end ia vivo - dio
-var nome = "Arthur"
-var chatbot = "inteligência artificial"
-var canal = "WhatsApp"
+# VARIÁVEIS
+# Nome de uma pessoa
+nome = "Arthur"
+# Tipo de tecnologia
+chatbot = "inteligência artificial"
+# Canal de comunicação
+canal = "WhatsApp"
+# Idade de uma pessoa
+idade = 28
+# Saldo em uma conta bancária
+saldo_bancario = 1500.75
+
+#CONSTANTES
+# Valor de Pi
+PI = 3.14159
+# Velocidade da luz em m/s
+VELOCIDADE_DA_LUZ = 299792458
+# Temperatura máxima permitida
+TEMPERATURA_MAXIMA = 100
+# URL de uma API
+URL_API = "https://api.example.com/v1"
+# Taxa de conversão do dólar
+TAXA_CONVERSAO_DOLAR = 5.25
